1cc7cb403e5fd2001884a877690ae1affebb9c31
b <598449667@qq.com>
2021-01-25 11:29:28 +0800

    mod

--- movie-vue-web/vue.config.js
+++ movie-vue-web/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     proxy: { //匹配规则
       '/api': {
         //要访问的跨域的域名
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8081',
         ws: true,
         secure:false, // 使用的是http协议则设置为false，https协议则设置为true
         changOrigin: true, //开启代理
